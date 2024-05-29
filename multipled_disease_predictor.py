f13d2c016f98936e224474fd109f690ef4a02acf
Apoorva .S. Mehta <71769587+apoorva1823000@users.noreply.github.com>
2024-05-29 08:49:15 +0530

    Update multipled_disease_predictor.py

--- multipled_disease_predictor.py
+++ multipled_disease_predictor.py
@@ -36,7 +36,7 @@ st.markdown(
         border-radius: 10px;
         padding: 10px;
     }
-    .stSideBar{
+    .stSidebar{
         background: linear-gradient(to bottom, #020024, #090979);
         border-radius: 15px;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
